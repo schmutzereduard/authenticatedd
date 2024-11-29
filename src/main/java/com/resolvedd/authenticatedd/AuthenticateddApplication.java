eb1e128538535d33e55769d78e57327e5cc1ece9
schmutzereduard <120651606+schmutzereduard@users.noreply.github.com>
2024-11-29 20:30:54 +0200

    First commit

---
+++ src/main/java/com/resolvedd/authenticatedd/AuthenticateddApplication.java
@@ -0,0 +1,13 @@
+package com.resolvedd.authenticatedd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticateddApplication.class, args);
+	}
+
+}
